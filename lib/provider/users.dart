d53bfb2f7000e398ec3afe13c91e133bdf45d548
Allison Piovani <apiovani25@gmail.com>
2020-07-05 16:30:17 -0300

    remove working

--- lib/provider/users.dart
+++ lib/provider/users.dart
@@ -43,4 +43,11 @@ class Users with ChangeNotifier {
 
     notifyListeners();
   }
+
+  void remove(User user) {
+    if (user != null && user.id != null) {
+      _items.remove(user.id);
+      notifyListeners();
+    }
+  }
 }
