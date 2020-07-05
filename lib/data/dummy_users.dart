064d95b1b16743e319ab88b1d643a8cdcd99ce5e
Allison Piovani <apiovani25@gmail.com>
2020-07-05 14:58:03 -0300

    Init user and perfil

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(id: '1', name: 'Maria', email: 'maria@gmail.com', avatarUrl: 'asserts/perfils/perfil-5.jpg'),
+  '2': const User(id: '2', name: 'Rafael', email: 'rafael@gmail.com', avatarUrl: 'asserts/perfils/perfil-1.jpg'),
+  '3': const User(id: '3', name: 'Gabriela', email: 'gabriela@gmail.com', avatarUrl: 'asserts/perfils/perfil-4.jpg'),
+  '4': const User(id: '4', name: 'Liliane', email: 'lili@gmail.com', avatarUrl: 'asserts/perfils/perfil-6.jpg'),
+  '5': const User(id: '5', name: 'Oscar', email: 'oscar@gmail.com', avatarUrl: 'asserts/perfils/perfil-2.jpg'),
+  '6': const User(id: '6', name: 'Lucas', email: 'lucas@gmail.com', avatarUrl: 'asserts/perfils/perfil-.jpg'),
+}
