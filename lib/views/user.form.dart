e1d65dc5501d060ee9c3a089fecdd6d2aa829efd
Allison Piovani <apiovani25@gmail.com>
2020-07-05 18:51:02 -0300

    Save new user

--- lib/views/user.form.dart
+++ lib/views/user.form.dart
@@ -1,8 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_crud/models/user.dart';
+import 'package:flutter_crud/provider/users.dart';
+import 'package:provider/provider.dart';
 
 class UserForm extends StatelessWidget {
+  final _form = GlobalKey<FormState>();
+  final Map<String, String> _formData = {};
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('Formulário de Usuário'),),);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Formulário de Usuário'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.save),
+            onPressed: () {
+              final isValid = _form.currentState.validate();
+
+              if (isValid) {
+                _form.currentState.save();
+
+                Provider.of<Users>(context, listen: false).put(
+                  User(
+                      id: _formData['id'],
+                      name: _formData['name'],
+                      email: _formData['email'],
+                      avatarUrl: _formData['avatarUrl']),
+                );
+
+                Navigator.of(context).pop();
+              }
+            },
+          )
+        ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(15),
+        child: Form(
+          key: _form,
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Name'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Ocorreu um erro';
+                  }
+                },
+                onSaved: (value) => _formData['name'] = value,
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'E-Mail'),
+                onSaved: (value) => _formData['email'] = value,
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Url do Avatar'),
+                onSaved: (value) => _formData['avatarUrl'] = value,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
