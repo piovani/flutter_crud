35fbadd85125a7324acfe4c2a856caa052f6aa03
Allison Piovani <apiovani25@gmail.com>
2020-07-05 15:41:09 -0300

    Add buttons to edit, delete and add new

--- lib/data/dummy_users.dart
+++ lib/data/dummy_users.dart
@@ -1,10 +1,10 @@
 import 'package:flutter_crud/models/user.dart';
 
 const DUMMY_USERS = {
-  '1': const User(id: '1', name: 'Maria', email: 'maria@gmail.com', avatarUrl: 'asserts/perfils/perfil-5.jpg'),
-  '2': const User(id: '2', name: 'Rafael', email: 'rafael@gmail.com', avatarUrl: 'asserts/perfils/perfil-1.jpg'),
-  '3': const User(id: '3', name: 'Gabriela', email: 'gabriela@gmail.com', avatarUrl: 'asserts/perfils/perfil-4.jpg'),
-  '4': const User(id: '4', name: 'Liliane', email: 'lili@gmail.com', avatarUrl: 'asserts/perfils/perfil-6.jpg'),
-  '5': const User(id: '5', name: 'Oscar', email: 'oscar@gmail.com', avatarUrl: 'asserts/perfils/perfil-2.jpg'),
-  '6': const User(id: '6', name: 'Lucas', email: 'lucas@gmail.com', avatarUrl: 'asserts/perfils/perfil-.jpg'),
-}
+  '1': const User(id: '1', name: 'Maria', email: 'maria@gmail.com', avatarUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRNy1HvnY-bFMf4xFX7n5QaIYrpb2QKVJl70w&usqp=CAU'),
+  '2': const User(id: '2', name: 'Rafael', email: 'rafael@gmail.com', avatarUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQoHYtXTchhspak0O8PNPKAPD9Cf08U6284ng&usqp=CAU'),
+  '3': const User(id: '3', name: 'Gabriela', email: 'gabriela@gmail.com', avatarUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTi4DLlHmETMFZYXRjqJ0-_mqTL97cz4lKVhg&usqp=CAU'),
+  '4': const User(id: '4', name: 'Liliane', email: 'lili@gmail.com', avatarUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTgtolb-Ib60scsv7o2YtxJZU2VLDWudTKZ6g&usqp=CAU'),
+  '5': const User(id: '5', name: 'Oscar', email: 'oscar@gmail.com', avatarUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR5IzyhF49KlM9TttZhS94nRfn7BmJN-cccAQ&usqp=CAU'),
+  '6': const User(id: '6', name: 'Lucas', email: 'lucas@gmail.com', avatarUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQnGXhf5TAKZl6dZtSjdBEeYllL_4C8yHIu-g&usqp=CAU'),
+};
